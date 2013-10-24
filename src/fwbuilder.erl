9e14bae03d6dd117a8dd5326658efb88fa1b8a71
Frank Hunleth <fhunleth@troodon-software.com>
2013-10-24 14:18:16 -0400

    Switch instruction list to JSON, start programmer

--- src/fwbuilder.erl
+++ src/fwbuilder.erl
@@ -9,50 +9,71 @@ main() ->
     MbrB = mbr_b(Options),
     RootFsPath = proplists:get_value(rootfs_path, Options),
     {ok, RootFs} = file:read_file(RootFsPath),
-    Manifest = [{"data/boot.img", proplists:get_value(boot_partition_start, Options)},
-		{"data/mbr-a.img", 0},
-		{"data/mbr-b.img", 0},
-		{"data/rootfs.img", proplists:get_value(rootfs_a_partition_start, Options)},
-		{"data/rootfs.img", proplists:get_value(rootfs_b_partition_start, Options)}],
-    ManifestBin = list_to_binary(lists:flatten(io_lib:print(Manifest))),
+    build_fw_file(Options, BootFs, MbrA, MbrB, RootFs),
+    build_image(Options, BootFs, MbrA, MbrB, RootFs).
 
-    FileList = [{"manifest", ManifestBin},
+in_blocks(What, Options) ->
+    proplists:get_value(What, Options).
+in_bytes(What, Options) ->
+    512 * in_blocks(What, Options).
+
+build_fw_file(Options, BootFs, MbrA, MbrB, RootFs) ->
+    Instructions = [{bootloader, 
+		     [[<<"pwrite">>, <<"data/boot.img">>, in_bytes(boot_partition_start, Options), byte_size(BootFs)]]},
+		    {update_a, 
+		     [[<<"pwrite">>, <<"data/rootfs.img">>, in_bytes(rootfs_a_partition_start, Options), byte_size(RootFs)],
+		      [<<"pwrite">>, <<"data/mbr-a.img">>, 0, byte_size(MbrA)]]},
+		    {update_b, 
+		     [[<<"pwrite">>, <<"data/rootfs.img">>, in_bytes(rootfs_b_partition_start, Options), byte_size(RootFs)],
+		      [<<"pwrite">>, <<"data/mbr-b.img">>, 0, byte_size(MbrB)]]}
+		   ],
+    InstructionsBin = jsx:encode(Instructions, [space, indent]),
+    io:format("~p~n", [InstructionsBin]),
+    FileList = [{"instructions.json", InstructionsBin},
 		{"data/boot.img", BootFs},
 		{"data/mbr-a.img", MbrA},
 		{"data/mbr-b.img", MbrB},
 	        {"data/rootfs.img", RootFs}],
-    {ok, _} = zip:create("test.fw", FileList).
+    {ok, _} = zip:create("test.fw", FileList).   
+
+build_image(Options, BootFs, MbrA, _MbrB, RootFs) ->
+    {ok, ImageFile} = file:open("test.img", [write]),
+    ok = file:pwrite(ImageFile, 0, MbrA),
+    ok = file:pwrite(ImageFile, in_bytes(boot_partition_start, Options), BootFs),
+    ok = file:pwrite(ImageFile, in_bytes(rootfs_a_partition_start, Options), RootFs),
+    ok = file:pwrite(ImageFile, in_bytes(rootfs_b_partition_start, Options), RootFs),
+    ok = file:close(ImageFile).
 
 % Build the MBR for booting off partition A
 mbr_a(Options) ->
-    mbr:create([{boot, fat12, proplists:get_value(boot_partition_start, Options), 
-		 proplists:get_value(boot_partition_count, Options)}, 
-		{normal, linux, proplists:get_value(rootfs_a_partition_start, Options), 
-		 proplists:get_value(rootfs_a_partition_count, Options)}, 
-		{normal, linux, proplists:get_value(rootfs_b_partition_start, Options),
-		 proplists:get_value(rootfs_b_partition_count, Options)}, 
-		{normal, linux, proplists:get_value(application_partition_start, Options),
-		 proplists:get_value(application_partition_count, Options)}]).
+    mbr:create([{boot, fat12, in_blocks(boot_partition_start, Options), 
+		 in_blocks(boot_partition_count, Options)}, 
+		{normal, linux, in_blocks(rootfs_a_partition_start, Options), 
+		 in_blocks(rootfs_a_partition_count, Options)}, 
+		{normal, linux, in_blocks(rootfs_b_partition_start, Options),
+		 in_blocks(rootfs_b_partition_count, Options)}, 
+		{normal, linux, in_blocks(application_partition_start, Options),
+		 in_blocks(application_partition_count, Options)}]).
 
 % Build the MBR for booting off partition B
 mbr_b(Options) ->
-    mbr:create([{boot, fat12, proplists:get_value(boot_partition_start, Options), 
-		 proplists:get_value(boot_partition_count, Options)}, 
-		{normal, linux, proplists:get_value(rootfs_b_partition_start, Options),
-		 proplists:get_value(rootfs_b_partition_count, Options)}, 
-		{normal, linux, proplists:get_value(rootfs_a_partition_start, Options), 
-		 proplists:get_value(rootfs_a_partition_count, Options)}, 
-		{normal, linux, proplists:get_value(application_partition_start, Options),
-		 proplists:get_value(application_partition_count, Options)}]).
+    mbr:create([{boot, fat12, in_blocks(boot_partition_start, Options), 
+		 in_blocks(boot_partition_count, Options)}, 
+		{normal, linux, in_blocks(rootfs_b_partition_start, Options),
+		 in_blocks(rootfs_b_partition_count, Options)}, 
+		{normal, linux, in_blocks(rootfs_a_partition_start, Options), 
+		 in_blocks(rootfs_a_partition_count, Options)}, 
+		{normal, linux, in_blocks(application_partition_start, Options),
+		 in_blocks(application_partition_count, Options)}]).
 
 % Build the boot file system
 boot_fs(Options) ->
     TmpFilename = "/tmp/boot.vfat",
     ok = subprocess:run("dd", ["if=/dev/zero", "of=" ++ TmpFilename, "count=0", 
-			  "seek=" ++ integer_to_list(proplists:get_value(boot_partition_count, Options))]),
+			  "seek=" ++ integer_to_list(in_blocks(boot_partition_count, Options))]),
     ok = subprocess:run("mkfs.vfat", ["-F", "12", "-n", "boot", TmpFilename]),
-    ok = subprocess:run("mcopy", ["-i", TmpFilename, proplists:get_value(mlo_path, Options), "::MLO"]),
-    ok = subprocess:run("mcopy", ["-i", TmpFilename, proplists:get_value(uboot_path, Options), "::U-BOOT.IMG"]),
+    ok = subprocess:run("mcopy", ["-i", TmpFilename, in_blocks(mlo_path, Options), "::MLO"]),
+    ok = subprocess:run("mcopy", ["-i", TmpFilename, in_blocks(uboot_path, Options), "::U-BOOT.IMG"]),
     {ok, Contents} = file:read_file(TmpFilename),
     ok = file:delete(TmpFilename),
     Contents.
---
+++ src/fwprogram.erl
@@ -0,0 +1,33 @@
+-module(fwprogram).
+
+-export([program/3]).
+
+-export([main/0]).
+
+main() ->
+    program("test.fw", update_a, "image.img").
+
+program(FirmwarePath, UpdateType, DestinationPath) ->
+    {ok, Destination} = file:open(DestinationPath, [write]),
+    {ok, ZipHandle} = zip:zip_open(FirmwarePath, [memory]),
+    {ok, {_, InstructionsBin}} = zip:zip_get("instructions.json", ZipHandle),
+    Instructions = jsx:decode(InstructionsBin, [{labels, atom}]),
+
+    case proplists:get_value(UpdateType, Instructions) of
+	undefined -> exit(unsupported_upgrade);
+	Update -> run_upgrade(Update, ZipHandle, Destination)
+    end,
+
+    ok = file:close(Destination),
+    ok = zip:zip_close(ZipHandle).
+
+run_command([<<"pwrite">>, Path, Location, Size], ZipHandle, Destination) ->
+    {ok, {_, Data}} = zip:zip_get(binary_to_list(Path), ZipHandle),
+    % Verify the size
+    Size = byte_size(Data),
+    ok = file:pwrite(Destination, Location, Data).
+
+run_upgrade([], _ZipHandle, _Destination) -> ok;
+run_upgrade([Command|T], ZipHandle, Destination) ->
+    run_command(Command, ZipHandle, Destination),
+    run_upgrade(T, ZipHandle, Destination).
