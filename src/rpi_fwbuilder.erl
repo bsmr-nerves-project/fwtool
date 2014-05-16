515ef66e0951e4dd9adeb8f346a2e6f85b1d23af
Frank Hunleth <fhunleth@troodon-software.com>
2014-05-16 13:18:46 -0400

    Remove use of config.txt for fwupdate on rpi
    config.txt modifications are needed per project, so it is nice to have
    this file be project modifiable again.

--- src/rpi_fwbuilder.erl
+++ src/rpi_fwbuilder.erl
@@ -29,12 +29,11 @@
 -spec build([{atom(), term()}]) -> ok.
 build(Options) ->
     BootFs = boot_fs(Options),
-    Mbr = mbr(Options),
     RootFsPath = get_path(rootfs_path, Options),
     {ok, RootFs} = file:read_file(RootFsPath),
     KernelPath = get_path(kernel_path, Options),
     {ok, Kernel} = file:read_file(KernelPath),
-    build_fw_file(Options, BootFs, Mbr, Kernel, RootFs).
+    build_fw_file(Options, BootFs, Kernel, RootFs).
 
 -spec in_blocks(atom(), [{atom(), term()}]) -> non_neg_integer().
 in_blocks(What, Options) ->
@@ -43,53 +42,43 @@ in_blocks(What, Options) ->
 in_bytes(What, Options) ->
     512 * in_blocks(What, Options).
 
-% Build the config.txt file contents that are used
-% to determine which Linux kernel to boot.
--spec build_config_file(string(), integer()) -> binary().
-build_config_file(Kernel, Partition) ->
-    ConfigTxt =
-	"# DO NOT EDIT. USED BY NERVES FOR FIRMWARE UPDATE\n"
-	"cmdline=\"dwc_otg.fiq_fix_enable=1 sdhci-bcm2708.sync_after_dma=0 dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p" ++ integer_to_list(Partition) ++ " rootwait -u\"\n"
-	"arm_freq=700\n"
-	"core_freq=250\n"
-	"kernel=" ++ Kernel ++ "\n"
-	"disable_overscan=1\n"
-	"gpu_mem_256=100\n"
-	"gpu_mem_512=100\n"
-	"sdram_freq=400\n"
-	"over_voltage=0\n",
-    list_to_binary(ConfigTxt).
-
--spec build_fw_file([{atom(), term()}], binary(), binary(), binary(), binary()) -> ok.
-build_fw_file(Options, BootFs, Mbr, Kernel, RootFs) ->
+-spec build_fw_file([{atom(), term()}], binary(), binary(), binary()) -> ok.
+build_fw_file(Options, BootFs, Kernel, RootFs) ->
+    MbrA = mbr_a(Options),
+    MbrB = mbr_b(Options),
     Instructions = [{bootloader,
-		     [[<<"pwrite">>, <<"data/mbr.img">>, 0, byte_size(Mbr)],
-		      [<<"pwrite">>, <<"data/boot.img">>, in_bytes(boot_partition_start, Options), byte_size(BootFs)]]},
+		     [[<<"pwrite">>, <<"data/mbr-a.img">>, 0, byte_size(MbrA)],
+		      [<<"pwrite">>, <<"data/boot.img">>, in_bytes(boot_partition_start, Options), byte_size(BootFs)],
+		      [<<"fat_write_file">>, <<"data/zImage">>, in_bytes(boot_partition_start, Options), <<"zImage">>]]},
 		    {update_a,
 		     [[<<"pwrite">>, <<"data/rootfs.img">>, in_bytes(rootfs_a_partition_start, Options), byte_size(RootFs)],
-		      [<<"fat_write_file">>, <<"data/zImage">>, in_bytes(boot_partition_start, Options), <<"zImage.a">>],
-		      [<<"fat_write_file">>, <<"data/config.a.txt">>, in_bytes(boot_partition_start, Options), <<"config.txt">>]]},
+		      [<<"fat_rm_file">>, in_bytes(boot_partition_start,Options), <<"zImage.previous">>],
+		      [<<"fat_write_file">>, <<"data/zImage">>, in_bytes(boot_partition_start, Options), <<"zImage.next">>],
+		      [<<"fat_mv_file">>, in_bytes(boot_partition_start,Options), <<"zImage">>, <<"zImage.previous">>],
+		      [<<"fat_mv_file">>, in_bytes(boot_partition_start,Options), <<"zImage.next">>, <<"zImage">>],
+		      [<<"pwrite">>, <<"data/mbr-a.img">>, 0, byte_size(MbrA)]]},
 		    {update_b,
 		     [[<<"pwrite">>, <<"data/rootfs.img">>, in_bytes(rootfs_b_partition_start, Options), byte_size(RootFs)],
-		      [<<"fat_write_file">>, <<"data/zImage">>, in_bytes(boot_partition_start, Options), <<"zImage.b">>],
-		      [<<"fat_write_file">>, <<"data/config.b.txt">>, in_bytes(boot_partition_start, Options), <<"config.txt">>]]},
+		      [<<"fat_rm_file">>, in_bytes(boot_partition_start,Options), <<"zImage.previous">>],
+		      [<<"fat_write_file">>, <<"data/zImage">>, in_bytes(boot_partition_start, Options), <<"zImage.next">>],
+		      [<<"fat_mv_file">>, in_bytes(boot_partition_start,Options), <<"zImage">>, <<"zImage.previous">>],
+		      [<<"fat_mv_file">>, in_bytes(boot_partition_start,Options), <<"zImage.next">>, <<"zImage">>],
+		      [<<"pwrite">>, <<"data/mbr-b.img">>, 0, byte_size(MbrB)]]},
 		    {autoupdate,
-		     [[<<"fat_compare_and_run">>, <<"data/config.a.txt">>, in_bytes(boot_partition_start, Options), <<"config.txt">>, <<"update_b">>],
-		      [<<"fat_compare_and_run">>, <<"data/config.b.txt">>, in_bytes(boot_partition_start, Options), <<"config.txt">>, <<"update_a">>],
+		     [[<<"compare_and_run">>, <<"data/mbr-a.img">>, 0, byte_size(MbrA), <<"update_b">>],
+		      [<<"compare_and_run">>, <<"data/mbr-b.img">>, 0, byte_size(MbrB), <<"update_a">>],
 		      [<<"fail">>, <<"Unexpected config.txt contents so not updating">>]]},
 		    {complete,
-		     [[<<"pwrite">>, <<"data/mbr.img">>, 0, byte_size(Mbr)],
+		     [[<<"pwrite">>, <<"data/mbr-a.img">>, 0, byte_size(MbrA)],
 		      [<<"pwrite">>, <<"data/boot.img">>, in_bytes(boot_partition_start, Options), byte_size(BootFs)],
+		      [<<"fat_write_file">>, <<"data/zImage">>, in_bytes(boot_partition_start, Options), <<"zImage">>],
 		      [<<"pwrite">>, <<"data/rootfs.img">>, in_bytes(rootfs_a_partition_start, Options), byte_size(RootFs)]]}
 		   ],
     InstructionsBin = jsx:encode(Instructions, [space, indent]),
-    ConfigA = build_config_file("zImage.a", 2),
-    ConfigB = build_config_file("zImage.b", 3),
     FileList = [{"instructions.json", InstructionsBin},
-		{"data/mbr.img", Mbr},
+		{"data/mbr-a.img", MbrA},
+		{"data/mbr-b.img", MbrB},
 		{"data/boot.img", BootFs},
-		{"data/config.a.txt", ConfigA},
-		{"data/config.b.txt", ConfigB},
 		{"data/zImage", Kernel},
 	        {"data/rootfs.img", RootFs}],
     OutputFile = proplists:get_value(firmware, Options),
@@ -97,14 +86,26 @@ build_fw_file(Options, BootFs, Mbr, Kernel, RootFs) ->
     ok.
 
 % Build the MBR
--spec mbr([{atom(),term()}]) -> binary().
-mbr(Options) ->
+-spec mbr_a([{atom(),term()}]) -> binary().
+mbr_a(Options) ->
     mbr:create(<<0:3520>>, % Bootstrap code unused.
 	       [{boot, fat32, in_blocks(boot_partition_start, Options),
 		 in_blocks(boot_partition_count, Options)},
 		{normal, linux, in_blocks(rootfs_a_partition_start, Options),
 		 in_blocks(rootfs_a_partition_count, Options)},
 		{normal, linux, in_blocks(rootfs_b_partition_start, Options),
+		 in_blocks(rootfs_b_partition_count, Options)},
+		{normal, linux, in_blocks(application_partition_start, Options),
+		 in_blocks(application_partition_count, Options)}]).
+
+-spec mbr_b([{atom(),term()}]) -> binary().
+mbr_b(Options) ->
+    mbr:create(<<0:3520>>, % Bootstrap code unused.
+	       [{boot, fat32, in_blocks(boot_partition_start, Options),
+		 in_blocks(boot_partition_count, Options)},
+		{normal, linux, in_blocks(rootfs_b_partition_start, Options),
+		 in_blocks(rootfs_a_partition_count, Options)},
+		{normal, linux, in_blocks(rootfs_a_partition_start, Options),
 		 in_blocks(rootfs_b_partition_count, Options)},
 		{normal, linux, in_blocks(application_partition_start, Options),
 		 in_blocks(application_partition_count, Options)}]).
@@ -153,14 +154,6 @@ boot_fs(Options) ->
     OtherFiles = proplists:get_value(boot_partition_extra_files, Options),
     [ copy_to_fat(BootFilename, abspath(Src, Options), Dst) || {Src, Dst} <- OtherFiles ],
 
-    KernelPath = get_path(kernel_path, Options),
-    copy_to_fat(BootFilename, KernelPath, "zImage.a"),
-
-    DefaultCfgFilename = tmpdir() ++ "/fwtool-config.txt",
-    ok = file:write_file(DefaultCfgFilename, build_config_file("zImage.a", 2)),
-    copy_to_fat(BootFilename, DefaultCfgFilename, "config.txt"),
-
     {ok, Contents} = file:read_file(BootFilename),
     ok = file:delete(BootFilename),
-    ok = file:delete(DefaultCfgFilename),
     Contents.
